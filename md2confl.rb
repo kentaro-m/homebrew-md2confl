b8d42fa442ce56dcc21adca070b3c1abd8a57fc6
Kentaro Matsushita <kentaro.m811@gmail.com>
2017-05-05 21:31:01 +0900

    Fix md2confl.rb

--- md2confl.rb
+++ md2confl.rb
@@ -5,7 +5,7 @@ class Md2confl < Formula
   url "https://github.com/kentaro-m/md2confl/releases/download/v0.1.2/darwin_amd64_md2confl"
   sha256 "694d07cbbe5555b9e95abdf201a8de604a49955a9691c54f42325a43f6786cf7"
   head "https://github.com/kentaro-m/md2confl.git"
-  version "0.1.2"
+  version "v0.1.2"
 
   def install
     system "mv", "darwin_amd64_md2confl", "md2confl"
