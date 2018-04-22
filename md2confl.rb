3c8170ab9a80e1cf8cde24170237b24b58ffe5a4
Kentaro Matsushita <kentaro.m811@gmail.com>
2018-04-22 22:33:02 +0900

    Release v0.2.0.

--- md2confl.rb
+++ md2confl.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Md2confl < Formula
   homepage "https://github.com/kentaro-m/md2confl"
-  url "https://github.com/kentaro-m/md2confl/releases/download/v0.1.2/darwin_amd64_md2confl"
+  url "https://github.com/kentaro-m/md2confl/releases/download/v0.2.0/darwin_amd64_md2confl"
   sha256 "25f9f63ed530160a8bb4fb67783796ffbbd53d08b5fb9b1e9ce3959d0adf6368"
   head "https://github.com/kentaro-m/md2confl.git"
-  version "v0.1.2"
+  version "v0.2.0"
 
   def install
     system "mv", "darwin_amd64_md2confl", "md2confl"
