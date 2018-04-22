d723bcee8a5dfad3b2b1a66ab7039043b67536a0
Kentaro Matsushita <kentaro.m811@gmail.com>
2018-04-22 23:16:59 +0900

    Update sha256 hash.

--- md2confl.rb
+++ md2confl.rb
@@ -3,7 +3,7 @@ require "formula"
 class Md2confl < Formula
   homepage "https://github.com/kentaro-m/md2confl"
   url "https://github.com/kentaro-m/md2confl/releases/download/v0.2.0/darwin_amd64_md2confl"
-  sha256 "25f9f63ed530160a8bb4fb67783796ffbbd53d08b5fb9b1e9ce3959d0adf6368"
+  sha256 "af563ed2518fb98edb2f20c2575b065ff7bcdef3cc4d712b64aab0d242ffbff5"
   head "https://github.com/kentaro-m/md2confl.git"
   version "v0.2.0"
 
