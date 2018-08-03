43b41de4b529bc879be4e8baa3b9d003ac76d1ba
Kentaro Matsushita <kentaro.m811@gmail.com>
2018-08-04 00:38:15 +0900

    release v0.3.0

--- md2confl.rb
+++ md2confl.rb
@@ -1,11 +1,12 @@
 require "formula"
 
 class Md2confl < Formula
+  desc "🛠md2confl is a CLI tool to convert the markdown text to confluence wiki format."
   homepage "https://github.com/kentaro-m/md2confl"
-  url "https://github.com/kentaro-m/md2confl/releases/download/v0.2.0/darwin_amd64_md2confl"
+  url "https://github.com/kentaro-m/md2confl/releases/download/v0.3.0/darwin_amd64_md2confl"
   sha256 "af563ed2518fb98edb2f20c2575b065ff7bcdef3cc4d712b64aab0d242ffbff5"
   head "https://github.com/kentaro-m/md2confl.git"
-  version "v0.2.0"
+  version "v0.3.0"
 
   def install
     system "mv", "darwin_amd64_md2confl", "md2confl"
